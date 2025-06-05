8a5ee807895bf01be498d29e1666dc41b6bae4fc
Aleksander Błaszkiewicz <kqmdjc8@gmail.com>
2025-06-05 23:23:09 +0200

    fix: fix warning level (#4)
    * fix: fix warning level
    
    * feat(): wip
    
    ---------
    
    Co-authored-by: Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>

--- Logdash/Models/LogLevel.cs
+++ Logdash/Models/LogLevel.cs
@@ -11,7 +11,7 @@ internal sealed class LogLevel(string value)
     public override string ToString() => Value;
     
     public static readonly LogLevel Error = new("error");
-    public static readonly LogLevel Warn = new("warn");
+    public static readonly LogLevel Warn = new("warning");
     public static readonly LogLevel Info = new("info");
     public static readonly LogLevel Http = new("http");
     public static readonly LogLevel Verbose = new("verbose");
